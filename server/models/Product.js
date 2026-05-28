930727ba860d45ba4aae0a6714eb1aa03515210e
NhanNghia2004 <22130185@st.hcmuaf.edu.vn>
2026-05-28 19:38:48 +0700

    update số lượng, list đặt gia công

--- server/models/Product.js
+++ server/models/Product.js
@@ -23,6 +23,11 @@ const Product = sequelize.define('Product', {
         type: DataTypes.STRING,
         allowNull: false
     },
+    quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0
+    },
     desc: {
         type: DataTypes.TEXT,
         allowNull: true
